e94f523bba0aa5dfeb50467e3fa3e8c6e3239e24
mobius-e1 <mobius@e1.ru>
2023-05-28 23:00:55 +0600

    +

--- js/ymaps.js
+++ js/ymaps.js
@@ -13,7 +13,7 @@ function init() {
         balloonContentBody: 'Художественная галерея Blanchard<br>Шоурум №4<br>Леонтьевский переулок, дом 5/1<br>',
     }, {
         iconLayout: 'default#image',
-        iconImageHref: '../img/mapmark.svg',
+        iconImageHref: '../img/events-768-5.jpg',
         iconImageSize: [20, 20],
         iconImageOffset: [-12, -1],
         hideIconOnBalloonOpen: false,
